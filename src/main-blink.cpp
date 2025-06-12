779a61d51dce4e6825aead5bc6bf996193481ce0
alifeee <alifeee.web@outlook.com>
2025-06-12 13:33:18 +0100

    format files with `clangd` extension

--- src/main-blink.cpp
+++ src/main-blink.cpp
@@ -4,33 +4,30 @@
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
- #include "Arduino.h"
+#include "Arduino.h"
 
- // Set LED_BUILTIN if it is not defined by Arduino framework
- // #define LED_BUILTIN 13
- #define LOGIC_PIN 2
- 
- void setup()
- {
-   // initialize LED digital pin as an output.
-   pinMode(LED_BUILTIN, OUTPUT);
-   pinMode(LOGIC_PIN, OUTPUT);
- }
- 
- void loop()
- {
-   // turn the LED OFF
-   digitalWrite(LED_BUILTIN, HIGH);
-   digitalWrite(LOGIC_PIN, HIGH);
- 
-   // wait for a second
-   delay(1000);
- 
-   // turn the LED ON by making the voltage LOW
-   digitalWrite(LED_BUILTIN, LOW);
-   digitalWrite(LOGIC_PIN, LOW);
- 
-   // wait for a second
-   delay(1000);
- }
- 
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 13
+#define LOGIC_PIN 2
+
+void setup() {
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LOGIC_PIN, OUTPUT);
+}
+
+void loop() {
+  // turn the LED OFF
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LOGIC_PIN, HIGH);
+
+  // wait for a second
+  delay(1000);
+
+  // turn the LED ON by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LOGIC_PIN, LOW);
+
+  // wait for a second
+  delay(1000);
+}
--- src/main-scrolling.cpp
+++ src/main-scrolling.cpp
@@ -1,4 +1,4 @@
- /*
+/*
  * Scrolling text
  * @alifeee
  */
@@ -12,28 +12,25 @@
 Panel topRow = Panel(LOAD_PIN, NUM_CHIPS);
 // Panel bottomRow = Panel(LOAD_PIN_2, NUM_CHIPS);
 
-void showText(Panel &p, char *msg)
-{
-    p.setCursor(0, 0);
-    p.setTextColor(1);
-    p.setTextSize(1); // pixel size multiplier
-    p.println(msg);
-    p.display();
+void showText(Panel &p, char *msg) {
+  p.setCursor(0, 0);
+  p.setTextColor(1);
+  p.setTextSize(1); // pixel size multiplier
+  p.println(msg);
+  p.display();
 }
 
-void setup()
-{
-    topRow.begin();
-    // bottomRow.begin();
+void setup() {
+  topRow.begin();
+  // bottomRow.begin();
 
-    showText(topRow, (char *)"this is a text");
-    // showText(bottomRow, (char *)"@alifeee");
+  showText(topRow, (char *)"this is a text");
+  // showText(bottomRow, (char *)"@alifeee");
 }
 
-void loop()
-{
-    topRow.scrollRows(-1, true); // wrap scroll left to right
-    topRow.display();
-    // bottomRow.scrollRows(-1, true); // wrap scroll right to left
-    // bottomRow.display();
+void loop() {
+  topRow.scrollRows(-1, true); // wrap scroll left to right
+  topRow.display();
+  // bottomRow.scrollRows(-1, true); // wrap scroll right to left
+  // bottomRow.display();
 }
