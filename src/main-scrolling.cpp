93badc60b45ec52b243c2f574ab15581a9981de1
alifeee <alifeee.web@outlook.com>
2025-07-31 13:26:12 +0100

    redefine code for Arduino :']

--- src/main-scrolling.cpp
+++ src/main-scrolling.cpp
@@ -10,7 +10,7 @@
 // panel length
 #define NUM_CHIPS 32
 Panel topRow = Panel(LOAD_PIN, NUM_CHIPS);
-// Panel bottomRow = Panel(LOAD_PIN_2, NUM_CHIPS);
+Panel bottomRow = Panel(LOAD_PIN_2, NUM_CHIPS);
 
 void showText(Panel &p, char *msg) {
   p.setCursor(0, 0);
@@ -23,10 +23,10 @@ void showText(Panel &p, char *msg) {
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   topRow.begin();
-  // bottomRow.begin();
+  bottomRow.begin();
 
   showText(topRow, (char *)"this is a text");
-  // showText(bottomRow, (char *)"@alifeee");
+  showText(bottomRow, (char *)"@alifeee");
 }
 
 void loop() {
@@ -34,6 +34,6 @@ void loop() {
   topRow.scrollRows(-1, true); // wrap scroll left to right
   topRow.display();
   digitalWrite(LED_BUILTIN, HIGH);
-  // bottomRow.scrollRows(-1, true); // wrap scroll right to left
-  // bottomRow.display();
+  bottomRow.scrollRows(1, true); // wrap scroll right to left
+  bottomRow.display();
 }
--- src/pins.h
+++ src/pins.h
@@ -1,12 +1,13 @@
 /* pin definitions
-*/
+ */
 
 // top panel
-#define LOAD_PIN 4 // blue/D2 on D1 mini
-// #define LOAD_PIN 7 // Arduino
+// #define LOAD_PIN 4 // blue/D2 on D1 mini
+#define LOAD_PIN 7 // Arduino
 
 // bottom panel
-#define LOAD_PIN_2 5 // orange/D1 on D1 mini
+// #define LOAD_PIN_2 5 // orange/D1 on D1 mini
+#define LOAD_PIN_2 5 // Arduino
 
 // panel length
 #define NUM_CHIPS 32
